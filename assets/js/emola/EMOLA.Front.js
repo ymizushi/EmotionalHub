8623ef218e3b4a6006df25e2f770a3365e7cf4ee
ymizushi <mizushi@gmail.com>
2014-08-07 14:57:38 +0900

    edit code

--- assets/js/emola/EMOLA.Front.js
+++ assets/js/emola/EMOLA.Front.js
@@ -6,9 +6,9 @@ function read_eval(input) {
 
 $(document).ready(function(){
    /* First console */
-   var console = $('<div class="console">');
-   $('body').append(console);
-   var controller1 = console.console({
+   var commandContainer = $('<div class="console">');
+   $('body').append(commandContainer);
+   var controller1 = commandContainer.console({
      promptLabel: 'Emola> ',
      commandValidate: function(line){
        if (line == "") return false;
