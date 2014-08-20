419041fb61ad6bf81c3e0c232356a03f11082410
ymizushi <mizushi@gmail.com>
2014-08-20 23:33:21 +0900

    add if

--- spec/List.IfSpec.js
+++ spec/List.IfSpec.js
@@ -1,8 +1,13 @@
 describe("List.If Spec", function() {
   it("integration", function() {
-      var typeIfList = new EMOLA.List.If([new EMOLA.Atom('if'), new EMOLA.Atom('true'), new EMOLA.Atom('int', 1), new EMOLA.Atom('false')]);
-    ;
-    expect(typeIfList.eval(EMOLA.Global.env)).toEqual(1);
+    var resultTrueTypeIfList = new EMOLA.List.If([new EMOLA.Atom('if'), new EMOLA.Atom('true'), new EMOLA.Atom('int', 1), new EMOLA.Atom('false')]);
+    expect(resultTrueTypeIfList.eval(EMOLA.Global.env)).toEqual(1);
+
+    var resultFalseTypeIfList = new EMOLA.List.If([new EMOLA.Atom('if'), new EMOLA.Atom('false'), new EMOLA.Atom('int', 1), new EMOLA.Atom('false')]);
+    expect(resultFalseTypeIfList.eval(EMOLA.Global.env)).toEqual(false);
+
+    var resultTrueTypeIfNestedList = new EMOLA.List.If([new EMOLA.Atom('if'), new EMOLA.Atom('true'), new EMOLA.List.If([new EMOLA.Atom('if'), new EMOLA.Atom('true'), new EMOLA.Atom('int', 10), new EMOLA.Atom('false')]), new EMOLA.Atom('false')]);
+    expect(resultTrueTypeIfNestedList.eval(EMOLA.Global.env)).toEqual(10);
   });
 });
 
