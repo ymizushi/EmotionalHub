a95e0d34dc8f3a3f2209d0d539499c52be687022
ymizushi <mizushi@gmail.com>
2014-08-24 13:21:27 +0900

    add List.Minus

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,7 @@ module.exports = function(grunt) {
           'assets/js/emola/EMOLA.List.Do.js',
           'assets/js/emola/EMOLA.List.Def.js',
           'assets/js/emola/EMOLA.List.Plus.js',
+          'assets/js/emola/EMOLA.List.Minus.js',
           'assets/js/emola/EMOLA.Line.js',
           'assets/js/emola/EMOLA.TokenReader.js',
           'assets/js/emola/EMOLA.ContextWrapper.js',
