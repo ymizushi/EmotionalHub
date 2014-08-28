4744363b6ecaaef2719fbcddd6a4595ef8d5f717
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2014-08-28 23:02:20 +0900

    edit code

--- assets/js/emola/EMOLA.Fn.js
+++ assets/js/emola/EMOLA.Fn.js
@@ -4,9 +4,10 @@ EMOLA.Fn = function (args, exp, env) {
   this.env = env;
 };
 
-// EMOLA.Fn.prototype.exec = function (valueArgs, env) {
-//   for (var i=0;i<this.args.length;i++) {
-//     this.env.dict[this.args[i].value] = valueArgs[i].eval(this.env);
-//   }
-//   return this.exp.eval(this.env);
-// };
+EMOLA.List.Fn.prototype.eval = function (env) {
+  this.assert();
+  return this.exp.eval(this.env);
+}
+
+EMOLA.List.Fn.prototype.assert = function () {
+}
