4b1682cee75a1f465f1ede8d1b636756ca91976e
ymizushi <mizushi@gmail.com>
2014-09-02 01:51:06 +0900

    edit code

--- assets/js/emola/EMOLA.List.Eval.js
+++ assets/js/emola/EMOLA.List.Eval.js
@@ -6,6 +6,12 @@ EMOLA.List.Eval.prototype = new EMOLA.List();
 
 EMOLA.List.Eval.prototype.eval = function (env) {
   this.assert();
+  if (this.list[1].type === EMOLA.Atom.VAR) {
+    var value = this.list[1].value;
+    var quote = env.find(value).get(value);
+    return quote.eval(env);
+  
+  }
   return this.list[1].eval(env).eval(env);
 }
 
