e77709043f99dedf2196887cdf3674796ac88482
ymizushi <mizushi@gmail.com>
2014-09-24 23:35:22 +0900

    コーディングTypeを変更

--- emhub-js/assets/js/emola/EMOLA.Color.js
+++ emhub-js/assets/js/emola/EMOLA.Color.js
@@ -1,13 +1,19 @@
-EMOLA.Color = function (r, g, b, a) {
-  this.r = r||0;
-  this.g = g||0;
-  this.b = b||0;
-  this.a = a||1;
-};
-
-EMOLA.Color.prototype.move = function (color) {
-  this.r = color.r;
-  this.g = color.g;
-  this.b = color.b;
-  this.a = color.a;
-}
+var EMOLA;
+(function (EMOLA) {
+    var Color = (function () {
+        function Color(r, g, b, a) {
+            this.r = r||0;
+            this.g = g||0;
+            this.b = b||0;
+            this.a = a||0;
+        }
+        Color.prototype.move = function (color) {
+            this.r = color.r;
+            this.g = color.g;
+            this.b = color.b;
+            this.a = color.a;
+        };
+        return Color;
+    })();
+    EMOLA.Color = Color;
+})(EMOLA || (EMOLA = {}));
