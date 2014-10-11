6a4ebde04245336bca693d7fc7a65309211ead25
ymizushi <mizushi@gmail.com>
2014-10-12 03:31:18 +0900

    edit code

--- emhub-js/assets/typescript/emola/EMOLA.ts
+++ emhub-js/assets/typescript/emola/EMOLA.ts
@@ -637,20 +637,25 @@ module EMOLA {
     }
   }
   
-  EMOLA.Socket = function () {
-    var socket = new WebSocket("ws://localhost:5000");
-    socket.onopen = function (event) {
-      console.log("web socket connection is established.");
-    };
-    socket.onmessage = function (event) {
-      console.log(event.data);
-    };
-    this.socket = socket;
-  };
-  
-  EMOLA.Socket.prototype.send = function (message) {
-    this.socket.send(message);
-  };
+
+  class Socket {
+    socket: any
+
+    constructor() {
+      var socket = new WebSocket("ws://localhost:5000")
+      socket.onopen = function (event) {
+        console.log("web socket connection is established.")
+      }
+      socket.onmessage = function (event) {
+        console.log(event.data)
+      }
+      this.socket = socket
+    }
+    
+    send(message) {
+      this.socket.send(message)
+    }
+  }
   
   EMOLA.Global = {};
   EMOLA.Global.env = new DictEnv(null);
