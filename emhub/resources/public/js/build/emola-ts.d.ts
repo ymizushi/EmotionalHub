2deaff594a29b46ca2da82033a7ccc5b84e92d77
ymizushi <mizushi@gmail.com>
2014-11-26 22:04:23 +0900

    rename

--- emhub/resources/public/js/build/emola-ts.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../../typings/jquery/jquery.d.ts" />
-declare var EMOLA: any;
-declare var EMOLA: any;
-declare function getDrawing(drawing?: any): any;
-declare function getPosition(): any;
-declare var EMOLA: any;
