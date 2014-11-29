aa5b8ab8b75a4dfc64669b82f0d5e9ed386270a9
ymizushi <mizushi@gmail.com>
2014-11-29 06:57:15 -0500

    edit setup code

---
+++ emhub/emhub-js/setup_js.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ `which brew` ]; then
+    brew install npm
+elif [ `which apt-get`  ]; then
+    sudo apt-get -y install npm
+elif [ `which yum`  ]; then
+    sudo yum -y install npm
+else
+    echo "npm install command is not found."
+    exit 1
+fi
+sudo npm link
+sudo npm install -g bower
+bower install
+npm install -g grunt
+exit 0
